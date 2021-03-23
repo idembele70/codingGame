54a803e3906936db0b34eb7d7a6507c3b31252f0
idembele70 <idembele70@gmail.com>
2021-03-23 23:40:58 +0100

    clash of code

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-let str = "", res = '';
+/* let str = "", res = '';
 function CaractereEnBinaire(char = 'Ch') {
     for (const i of char) {
         str += char.charCodeAt(i).toString(2);
@@ -13,4 +13,29 @@ function CaractereEnBinaire(char = 'Ch') {
 }
 console.log(CaractereEnBinaire());
 console.log("%".charCodeAt().toString(2));  
+  */
+
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+// reverse Mode
+ const L = parseInt(readline());
+ const M = parseInt(readline());
+ const N = parseInt(readline());
+ const tabsLineOne = [], tabsLineTwo = [];
+ tabsLineOne.push(L);
+ tabsLineTwo.push(L);
+ let valL = L; let valM = L;
+ for(let i = 1; i < N; i++){
+     valL+=M;
+     tabsLineOne.push(valL);
+     valM/=M;
+     tabsLineTwo.push(valM);
+ }
  
+ console.log(tabsLineOne.join(' '));
+ console.log(tabsLineTwo.join(' '));
+ 
+
