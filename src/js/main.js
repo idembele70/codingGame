bca753afd5ace82f4f0bbee2e65bd277f1e34a10
idembele70 <idembele70@gmail.com>
2021-04-06 11:34:46 +0200

    chuck noris

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,52 @@
-/* let str = "", res = '';
-function CaractereEnBinaire(char = 'Ch') {
-    for (const i of char) {
-        str += char.charCodeAt(i).toString(2);
-        console.log(str);
+
+/* const MESSAGE = "%%"
+let str = "", res = '', strNew = "",tr ="";
+
+for (let j = 0; j < MESSAGE.length; j++) {
+    str += MESSAGE.charCodeAt(j).toString(2);
+    if (str.length < 7) {
+        for (let i = 6; i >= 0; i--) {
+            strNew += str.charAt(i) !== "" ? str.charAt(i) : 0;
+        }
+    } else {
+        strNew = str;
     }
-    const reg0 = /0{1,}|1{1,}/gi;
-    str = str.match(reg0).map(x => {
-        return x[0] == 1 ? "0 " + "0".repeat(x.length) : "00 " + "0".repeat(x.length)
-    }).join(' ');
-    res+= str;
-    return res
+    tr+=strNew;
+    strNew ="";
 }
-console.log(CaractereEnBinaire());
-console.log("%".charCodeAt().toString(2));  
-  */
+console.log(tr);
+// console.log(strNew);
+const reg0 = /0{1,}|1{1,}/gi;
+str = tr.match(reg0).map(x => {
+    return x[0] == 1 ? "0 " + "0".repeat(x.length) : "00 " + "0".repeat(x.length)
+}).join(' ');
+res += str;
+console.log(res);
+console.log('%'.charAt(0).toString(2)); */
 
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 
+/*
+function Encode(MESSAGE = "%") {
+    var res = "";
+    var pre = -1;
 
-
-let premier = false, biggest = 0;
-for (let index = 0; index < M; index++) {
-    if (M % i == 0){
-    verify = true
+    for (let i = 0; i < MESSAGE.length; i++) {
+        for (let j = 6; j >= 0; j--) {
+            const bit = MESSAGE.charCodeAt(i) >> j & 1;
+            console.log(bit);
+            if (bit !== pre) {
+                if (-1 !== pre) {
+                    res += " ";
+                }
+                res+= 1 == bit ? "0 " : "00 ";
+                pre = bit;
+            }
+            res+="0"
+        }
     }
+    console.log(res);
 }
-if (biggest < M && !verify) {
-    biggest = M;
-}
+
+Encode() */
+
