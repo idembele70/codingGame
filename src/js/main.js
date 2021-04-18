d8ce90df29f3b3aae754be58ef15ba9f90088e3f
idembele70 <idembele70@gmail.com>
2021-04-18 19:47:47 +0200

    exercice

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,20 @@
-
-/* const MESSAGE = "%%"
-let str = "", res = '', strNew = "",tr ="";
-
-for (let j = 0; j < MESSAGE.length; j++) {
-    str += MESSAGE.charCodeAt(j).toString(2);
-    if (str.length < 7) {
-        for (let i = 6; i >= 0; i--) {
-            strNew += str.charAt(i) !== "" ? str.charAt(i) : 0;
-        }
-    } else {
-        strNew = str;
-    }
-    tr+=strNew;
-    strNew ="";
-}
-console.log(tr);
-// console.log(strNew);
-const reg0 = /0{1,}|1{1,}/gi;
-str = tr.match(reg0).map(x => {
-    return x[0] == 1 ? "0 " + "0".repeat(x.length) : "00 " + "0".repeat(x.length)
-}).join(' ');
-res += str;
-console.log(res);
-console.log('%'.charAt(0).toString(2)); */
-
-const Message = '%'
-let res = "";
-let pre = -1;
-const myBit = Message.charCodeAt(0).toString(2).padStart(7, 0);
-for (let i = 6; i >= 0; i--) {
-    const bit = Message.charCodeAt(0) >> i & 1;
-    if (bit != pre) {
-        if (-1 !== pre) {
-            res += " ";
-        }
-        res += 1 == bit ? "0 " : "00 ";
-        pre = bit
-    } res +=0
-}
-let nbr = 25;
-let nbr2 = (25).toString(2);
-console.log(nbr2);
-console.log(parseInt("01100",2));
-console.log(nbr >> 1); 
-
+const  N =3
+const Q = 11
+/* 
+Extensions :  wav audio/x-wav
+Extensions :  mp3 audio/mpeg
+Extensions :  pdf application/pdf
+ */
+/* 
+a             => UNKNOWN
+a.wav         => audio/ x-wav
+b.wav.tmp     => UNKNOWN
+test.vmp3     => UNKNOWN
+pdf           => UNKNOWN
+.pdf          => pdf
+mp3           => UNKNOWN
+report..pdf   => pdf
+defaultwav    => UNKNOWN
+.mp3.         => UNKNOWN
+final.        => UNKNOWN
+*/
