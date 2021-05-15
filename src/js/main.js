ed881fef9660831f91300cd03a1c198f4f64f558
idembele70 <idembele70@gmail.com>
2021-05-15 15:24:01 +0200

    clashofcode et kata

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
-const S = "heLLo world".toLowerCase().split('').forEach(el => el.replace(el[0],el[0].toUpperCase()))
-console.log(s); 
+function highAndLow(numbers = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") {
+  return Math.max.apply(0,[])
+}
+console.log(highAndLow());
+
+/* bois bandé 0675590818 */
