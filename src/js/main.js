13b1f991e48070bb628e9c68fa04779291db05d8
idembele70 <idembele70@gmail.com>
2021-05-20 01:19:36 +0200

    clash of code

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ function highAndLow(numbers = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") {
 console.log(highAndLow());
 
 /* bois bandé 0675590818 */
+n = 10
+let hauteur = 0;
+
+for(let i = 1; i <= n; i++) {
+    n -= i;
+    hauteur++;
+}
+
+console.log(hauteur, n);
