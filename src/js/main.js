5d5840684c8487f0458128793707fb9e84a27592
idembele70 <idembele70@gmail.com>
2021-05-23 00:19:03 +0200

    ANEO SPONSORED PUZZLE

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,36 @@
 function highAndLow(numbers = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") {
-  return Math.max.apply(0,[])
+  return Math.max.apply(0, []);
 }
-console.log(highAndLow());
+/* console.log(highAndLow()); */
 
 /* bois bandé 0675590818 */
-n = 10
+n = 10;
 let hauteur = 0;
 
-for(let i = 1; i <= n; i++) {
-    n -= i;
-    hauteur++;
+for (let i = 1; i <= n; i++) {
+  n -= i;
+  hauteur++;
 }
 
-console.log(hauteur, n);
+
+/*
+CONSIGNE :
+  Ligne 1 : Speed (Vmax) KM/H
+  Ligne 2 : nombre Feux
+  Ligne 3+ : Distance M + durée du feu S
+  
+  EXEMPLE 1 :
+  speed : 50
+  nb Feux : 1
+  distance : 200 + durée : 15 
+  (200/15*3.6) = 48
+
+
+  EXEMPLE 2 :
+  speed : 50
+  nb Feux : 1
+  distance : 200 + durée : 10
+  (200 / 10) * 3.6) = 72;
+  */
+console.log((200 / 10) * 3.6);
+console.log(5);
