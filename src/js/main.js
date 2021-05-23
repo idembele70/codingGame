4f1998b6d03aae19370be6f5a8da340a9ed8db4c
idembele70 <idembele70@gmail.com>
2021-05-24 00:25:30 +0200

    ANEO

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,6 @@ for (let i = 1; i <= n; i++) {
   hauteur++;
 }
 
-
 /*
 CONSIGNE :
   Ligne 1 : Speed (Vmax) KM/H
@@ -22,15 +21,36 @@ CONSIGNE :
   EXEMPLE 1 :
   speed : 50
   nb Feux : 1
-  distance : 200 + durée : 15 
+  distance : 200 + durée de feu : 15 
   (200/15*3.6) = 48
+  (200 / x * 3.6) = 50 ===> 200 / 50 * 3.6 = x = 14
+  (200 / x * 3.6) = 50 ===> 200 / 20 * 3.6 = x = x
 
 
   EXEMPLE 2 :
   speed : 50
   nb Feux : 1
-  distance : 200 + durée : 10
+  distance : 200 + durée de feu : 10
   (200 / 10) * 3.6) = 72;
+  (200 / x * 3.6) = 50 ===> 200 / 50 * 3.6 = x = 14
+  (200 / x * 3.6) = 50 ===> 200 / 20 * 3.6 = x = x
+
+
+  Speculation Reponse
+  variables VitesseMoyenne, compteur
+  on calcul TempsPourDepasseFeux =  distance / speed * 3.6
+    si TempsPourDepasseFeux > duréeFeux 
+    alors vitesseMoyenne += speed
+    compteur++
+    sinonSi TempsPourDepasseFeux < duréeFeux 
+    alors {
+      TempsPourDepasseFeux =(Math.ceil(TempsPourDepasseFeux/10))*10
+      const vNeccessaire = d / TempsPourDepasseFeux * 3.6
+      TempsPourDepasseFeux += vNeccessaire
+      compteur++
+    }
+  retourne vMoyenne / compteur
   */
-console.log((200 / 10) * 3.6);
+
+console.log((200 / 20) * 3.6);
 console.log(5);
